e562b6c6bc450bc1bd5030b9a208c96dc3a0cc1d
PHDMI <phdmi.sctimst@gmail.com>
2023-03-04 17:01:00 +0530

    Initial Commit

---
+++ github_setup_website.R
@@ -0,0 +1,5 @@
+usethis::use_git_config(user.name = "PHDMI", user.email = "phdmi.sctimst@gmail.com")
+usethis::create_github_token() 
+# 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
+credentials::set_github_pat("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")
+usethis::git_sitrep()
